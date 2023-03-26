e0abdc8e75bcd98143206526687ca3199ef3beae
Donglai Yang <dyang@wesleyan.edu>
2023-03-26 10:02:59 -0400

    more changes to plotting

--- testbed_ISSM/flatMISMIP_testbeds/Peclet_studies.m
+++ testbed_ISSM/flatMISMIP_testbeds/Peclet_studies.m
@@ -1,13 +1,25 @@
-load('long_models_yang/model_W11000_GL400_FC30000/MISMIP_yangTransient_Calving_MassUnloading.mat')
+load('long_models_yang/model_W5000_GL400_FC30000/MISMIP_yangTransient_Calving_MassUnloading.mat')
 
 % peclet number studies
+index = md.mesh.elements;
+meshx = md.mesh.x; meshy = md.mesh.y;
 nt = size(md.results.TransientSolution,2);
 ds = 50;
 smooth_span = 30;
-sampled_ti = 1:20:nt;
-[~,x,y] = mesh_to_grid(index, meshx, meshy, md.friction.C(1:end-1,i),ds);
+dt = 0.1; % dt in the simulation results
+t_interval = 2; % sample every 2 year
+ti_interval = floor(t_interval/dt);
+sampled_ti = 1:ti_interval:nt;
+[~,x,y] = mesh_to_grid(index, meshx, meshy, md.friction.C(1:end-1,1),ds);
+
 % pre-allocate
 Pes = zeros(length(sampled_ti), length(x));
+D0s = zeros(length(sampled_ti), length(x));
+dHs = zeros(length(sampled_ti), length(x));
+
+% thickness and basal coefficient at the steady state
+[H0,~,~] = mesh_to_grid(index, meshx, meshy, md.results.TransientSolution(1).Thickness,ds);
+[k0,~,~] = mesh_to_grid(index, meshx, meshy, md.friction.C(1:end-1,1),ds);
 
 count = 0;
 for i = sampled_ti
@@ -16,7 +28,6 @@ for i = sampled_ti
     index = md.mesh.elements;
     meshx = md.mesh.x;
     meshy = md.mesh.y;
-    [k,~,~] = mesh_to_grid(index, meshx, meshy, md.friction.C(1:end-1,i),ds);
     [H,~,~] = mesh_to_grid(index, meshx, meshy, md.results.TransientSolution(i).Thickness,ds);
     [mask,~,~] = mesh_to_grid(index, meshx, meshy, md.results.TransientSolution(i).MaskOceanLevelset,ds);
     % make floating parts nan
@@ -26,39 +37,70 @@ for i = sampled_ti
     [pw,~,~] = mesh_to_grid(index, meshx, meshy, pw, ds);
     [s,~,~] = mesh_to_grid(index, meshx, meshy, md.results.TransientSolution(i).Surface, ds);
     alpha = zeros(size(s));
-    smooth_n = 2000/ds;
+    smooth_n = 4500/ds; % Dennis used 10 times the ice thickness (here assuming h = 450)
     for j = 1:size(s,1)
         alpha(j,:) = smooth(gradient(s(j,:), ds), smooth_n);
     end
     % now calculate C0, D0, grad(D0)
-    C0 = compute_C0(k, H, alpha, pw);
-    D0 = compute_D0(k, H, alpha, pw);
+    C0 = compute_C0(k0, H, alpha, pw);
+    D0 = compute_D0(k0, H, alpha, pw);
     grad_D0 = zeros(size(D0));
     for j = 1:size(s,1)
         grad_D0(j,:) = smooth(gradient(D0(j,:), ds), smooth_n);
     end
-    %grad_D0 = compute_grad_D0(D0, ds);
-    % distance to ice front
+    % distance to ice front is the characteristic length in Peclet number
+    % calculation
     [distance,~,~] = mesh_to_grid(index, meshx, meshy, -1*md.results.TransientSolution(i).MaskIceLevelset, ds);
     distance(distance<0) = 0;
     Pe = (C0 - grad_D0)./D0.*distance;
     % Pe across-flow average
-    % the middle 20
+    % the middle 20 flowlines
     if rem(size(Pe,2), 1) == 0
         mid_i = size(Pe,1)/2;
     else
         mid_i = (size(Pe,1)+1)/2;
     end
-    % only look at the middle 20 lines
+    % only look at the middle 20 lines and behind the grounding line
     Pe(mask<0) = nan;
     Pes(count,:) = mean(Pe(mid_i-10:mid_i+10,:),1,'omitnan');
-    
+    D0s(count,:) = mean(D0(mid_i-10:mid_i+10,:),1,'omitnan');
+
+    % get the thickness change from the steady state 
+    dH = H-H0; dH(mask<0) = nan;
+    dHs(count,:) = dH(mid_i,:);
+
 end
+%% Recover the glacier profiles overtime
+[surface, base, bed, profiles_t] = sample_profile_evol(md, dt, ds, t_interval);
+
 %% plot
+% load colormap
+cp = load('plots/colormap/lajolla.mat').lajolla;
+
+% make the plot
 plot_x = 0:ds:(size(Pes,2)-1)*ds;
-figure; plot(plot_x/1000, Pes,'LineWidth',1);ylim([0,9]);colororder(cool(13))
-xlabel('Along flow distance (km)','FontName','Aria','FontSize',15)
+figure('Position',[100,100,900,600])
+tiledlayout(2,1,"TileSpacing","none")
+nexttile
+plot(x/1000, surface); hold on;
+plot(x/1000, base); hold on;
+plot(x/1000, bed,'-k'); hold off
+ylim([-600,1300])
+line_colors = colormap_to_colororder(cp, size(surface,1),1,100);
+colororder(line_colors)
+
+nexttile
+yyaxis left % plot the Peclet number
+plot(plot_x/1000, Pes,'LineWidth',1);
 ylabel('Peclet Number','FontName','Aria','FontSize',15)
+ylim([0,10]); set(gca,'XColor',[0 0 0]); set(gca,'YColor',[0 0 0]);
+colororder(line_colors)
+yyaxis right % plot dH
+plot(plot_x/1000, dHs, '-.','LineWidth',1);
+ylabel('dH (m)','FontName','Aria','FontSize',15)
+xlabel('Along flow distance (km)','FontName','Aria','FontSize',15)
+set(gca,'XColor',[0 0 0]); set(gca,'YColor',[0 0 0]);
+colororder(line_colors)
 plotname = ['Pe_evolv_', md.miscellaneous.name(9:end),'.png'];
 exportgraphics(gcf, ['plots/', plotname], 'Resolution',400)
 
@@ -66,7 +108,7 @@ exportgraphics(gcf, ['plots/', plotname], 'Resolution',400)
 function C0 = compute_C0(k, H, alpha, pw)
     m = 1;
     n = 3;
-    C0 = k.*((H.*alpha).^n./(H - pw).^m).*(1 + (n-m)*(H - n*pw)./(H - pw));
+    C0 = k.*((H.*alpha).^n./(H - pw).^m).*(1 + ((n-m)*H - n*pw)./(H - pw));
 end
 
 function D0 = compute_D0(k, H, alpha, pw)
